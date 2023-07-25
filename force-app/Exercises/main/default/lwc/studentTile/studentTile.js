2670b6086c074723e6dcedbb45bc24d9dbf57c63
saba airani <saba.airania@gmail.com>
2023-07-25 12:38:55 +0530

    2.05 change

--- force-app/Exercises/main/default/lwc/studentTile/studentTile.js
+++ force-app/Exercises/main/default/lwc/studentTile/studentTile.js
@@ -9,4 +9,8 @@ export default class StudentTile extends LightningElement {
   get tileSelected() {
     return this.isSelected ? "tile selected" : "tile";
   }
+
+  studentClick(){
+    alert(this.student.Name);
+    }
 }
--- force-app/Exercises/main/default/lwc/studentTiles/studentTiles.js
+++ force-app/Exercises/main/default/lwc/studentTiles/studentTiles.js
@@ -1,3 +1,5 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
 
-export default class StudentTiles extends LightningElement {}
+export default class StudentTiles extends LightningElement {
+   @api studentList = [];
+}
